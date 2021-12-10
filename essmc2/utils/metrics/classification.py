7d400d114c23a0db7a982a3de954d53ebb1d7365
Logistic1994 <logistic1994@gmail.com>
2021-12-10 15:37:46 +0800

    Add new metric ensemble_accuracy

--- essmc2/utils/metrics/classification.py
+++ essmc2/utils/metrics/classification.py
@@ -17,29 +17,29 @@ class AccuracyMetric(object):
         self.maxk = max(self.topk)
 
     @torch.no_grad()
-    def __call__(self, results, targets, prefix="acc"):
+    def __call__(self, logits, labels, prefix="acc"):
         """ Compute Accuracy
         Args:
-            results (torch.Tensor or numpy.ndarray):
-            targets (torch.Tensor or numpy.ndarray):
+            logits (torch.Tensor or numpy.ndarray):
+            labels (torch.Tensor or numpy.ndarray):
             prefix (str): Prefix string of ret key, default is acc.
 
         Returns:
             A OrderedDict, contains accuracy tensors according to topk.
 
         """
-        assert self.maxk <= results.shape[-1]
+        assert self.maxk <= logits.shape[-1]
 
-        if isinstance(results, np.ndarray):
-            results = torch.from_numpy(results)
-        if isinstance(targets, np.ndarray):
-            targets = torch.from_numpy(targets)
+        if isinstance(logits, np.ndarray):
+            logits = torch.from_numpy(logits)
+        if isinstance(labels, np.ndarray):
+            labels = torch.from_numpy(labels)
 
-        batch_size = results.size(0)
+        batch_size = logits.size(0)
 
-        _, pred = results.topk(self.maxk, 1, True, True)
+        _, pred = logits.topk(self.maxk, 1, True, True)
         pred = pred.t()
-        corrects = pred.eq(targets.view(1, -1).expand_as(pred))
+        corrects = pred.eq(labels.view(1, -1).expand_as(pred))
 
         res = OrderedDict()
         for k in self.topk:
@@ -48,45 +48,55 @@ class AccuracyMetric(object):
         return res
 
 
-@METRICS.register_class("accuracyx2")
-class AccuracyMetricx2(object):
-    def __init__(self, topk=(1,)):
+@METRICS.register_class("ensemble_accuracy")
+class EnsembleAccuracyMetric(object):
+    def __init__(self, topk=(1,), ensemble_method="avg"):
         if isinstance(topk, int):
             topk = (topk,)
         self.topk = topk
         self.maxk = max(self.topk)
+        assert ensemble_method in ('avg', 'max'), f"Expected ensemble_method in ('avg', 'max'), got {ensemble_method}"
+        self.ensemble_method = ensemble_method
 
-    def __call__(self, logits_0, logits_1, targets):
+    @torch.no_grad()
+    def __call__(self, logits, labels, keys, prefix="acc"):
         """ Compute Accuracy
         Args:
-            logits_0 (torch.Tensor or numpy.ndarray):
-            logits_1 (torch.Tensor or numpy.ndarray)
-            targets (torch.Tensor or numpy.ndarray):
+            logits (torch.Tensor or numpy.ndarray):
+            labels (torch.Tensor or numpy.ndarray):
+            keys (List[str]): Keys to accumulate logits.
+            prefix (str): Prefix string of ret key, default is acc.
 
         Returns:
             A OrderedDict, contains accuracy tensors according to topk.
 
         """
-        assert self.maxk <= logits_0.shape[-1]
-        assert self.maxk <= logits_1.shape[-1]
-        assert targets.shape[-1] == 2
+        if isinstance(logits, np.ndarray):
+            logits = torch.from_numpy(logits)
+        if isinstance(labels, np.ndarray):
+            labels = torch.from_numpy(labels)
 
-        if isinstance(logits_0, np.ndarray):
-            logits_0 = torch.from_numpy(logits_0)
-        if isinstance(logits_1, np.ndarray):
-            logits_1 = torch.from_numpy(logits_1)
-        if isinstance(targets, np.ndarray):
-            targets = torch.from_numpy(targets)
+        agg_keys = list(set(keys))
+        keys = np.asarray(keys)
 
-        res = OrderedDict()
-        res0 = AccuracyMetric(topk=self.topk)(logits_0, targets[:, 0])
-        res1 = AccuracyMetric(topk=self.topk)(logits_1, targets[:, 1])
+        agg_logits = []  # N * Tensor([C])
+        agg_labels = []  # N * Tensor(scalar)
 
-        for key, value in res0.items():
-            key = key.replace("@", "_0@")
-            res[key] = value
+        for key in agg_keys:
+            key_index = np.where(keys == key)[0]
+            key_index = torch.from_numpy(key_index)
+            key_logits = logits[key_index]
 
-        for key, value in res1.items():
-            key = key.replace("@", "_1@")
-            res[key] = value
-        return res
+            if self.ensemble_method == "avg":
+                key_logit = torch.mean(key_logits, dim=0)
+            else:
+                key_logit, _ = torch.max(key_logit, dim=0)
+            key_label = labels[key_index[0]]
+
+            agg_logits.append(key_logit)
+            agg_labels.append(key_label)
+
+        agg_logits = torch.vstack(agg_logits)
+        agg_labels = torch.hstack(agg_labels)
+
+        return AccuracyMetric(self.topk)(agg_logits, agg_labels, prefix)
