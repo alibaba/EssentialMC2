2d0daf316f03575dbc0a0b5baeafbf5b44908205
Logistic1994 <logistic1994@gmail.com>
2022-01-29 01:02:31 +0800

    Remove pytorch-video-understanding

--- papers/pytorch-video-understanding/models/module_zoo/heads/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-# Copyright (C) Alibaba Group Holding Limited. 
-
-from .mosi_head import MoSIHeadJoint
-from .slowfast_head import SlowFastHead
-from .transformer_head import TransformerHead
-from .bmn_head import BaseBMN
--- papers/pytorch-video-understanding/models/module_zoo/stems/__init__.py
+++
@@ -1,3 +0,0 @@
-from .downsample_stem import DownSampleStem
-from .r2plus1d_stem import R2Plus1DStem
-from .embedding_stem import PatchEmbedStem, TubeletEmbeddingStem
