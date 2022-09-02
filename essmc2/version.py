c64e2c9edf32456c1fe7fad771a898159afaa4c1
Logistic1994 <logistic1994@gmail.com>
2022-09-02 11:17:39 +0800

    Upgrade version to 0.1.4

--- essmc2/version.py
+++ essmc2/version.py
@@ -1,6 +1,6 @@
 # Copyright 2021 Alibaba Group Holding Limited. All Rights Reserved.
 
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 
 version_info = tuple(int(x) for x in __version__.split(".")[0:3])
 
